c4158fc53a6eceff12d43f028937ad84ea5bd133
Rohit verma <132906863+rohit637869@users.noreply.github.com>
2025-07-18 10:38:13 +0530

    Server Error solve 1

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,2 +1,2 @@
-const mongoUri = 'mongodb+srv://rohit637869:OEGJOprjIKtaAzZN@jobboard.ibe6ykz.mongodb.net/?retryWrites=true&w=majority&appName=jobboard';
+const mongoUri = 'mongodb+srv://rohit637869:OEGJOprjIKtaAzZN@jobboard.ibe6ykz.mongodb.net/jobboard?retryWrites=true&w=majority&tls=true&appName=jobboard';
 module.exports =  mongoUri
