21d57ebc3f05e8f068a56b8be792194a0e695019
Rohit verma <132906863+rohit637869@users.noreply.github.com>
2025-07-18 11:02:54 +0530

    Modify mognourl

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,2 +1,4 @@
-const mongoUri = 'mongodb+srv://rohit637869:OEGJOprjIKtaAzZN@jobboard.ibe6ykz.mongodb.net/jobboard?retryWrites=true&w=majority&tls=true&appName=jobboard';
+// const mongoUri = 'mongodb+srv://rohit637869:OEGJOprjIKtaAzZN@jobboard.ibe6ykz.mongodb.net/jobboard?retryWrites=true&w=majority&tls=true&appName=jobboard';
+const mongoUri = 'mongodb+srv://rohit637869:OEGJOprjIKtaAzZN@jobboard.ibe6ykz.mongodb.net/?retryWrites=true&w=majority&appName=jobboard';
+
 module.exports =  mongoUri
